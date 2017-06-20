253b044079070aae64d1acd396fe139f3bbd8784
philhomic <philhomic@qq.com>
2017-06-20 17:06:17 +0800

    10-4 finished

--- eleme-app/src/components/ratings/ratings.vue
+++ eleme-app/src/components/ratings/ratings.vue
@@ -25,10 +25,10 @@
         </div>
       </div>
       <split></split>
-      <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings"></ratingselect>
+      <ratingselect :select-type="selectType" :only-content="onlyContent" :ratings="ratings" v-on:ratingtypeselect="ratingtypeselect" v-on:contenttoggle="contenttoggle"></ratingselect>
       <div class="rating-wrapper">
         <ul>
-          <li v-for="rating in ratings" class="rating-item">
+          <li v-for="rating in ratings" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar">
             </div>
@@ -41,7 +41,7 @@
               <p class="text">{{rating.text}}</p>
               <div class="recommend" v-show="rating.recommend && rating.recommend.length">
                 <span class="icon-thumb_up"></span>
-                <span v-for="item in rating.recommend">{{item}}</span>
+                <span v-for="item in rating.recommend" class="item">{{item}}</span>
               </div>
               <div class="time">
                 {{rating.rateTime | formatDate}}
@@ -58,8 +58,8 @@
   import BScroll from 'better-scroll';
   import star from '@/components/star/star';
   import split from '@/components/split/split';
-  import ratingselect from '@/components/ratingselect/ratingselect';
   import {formatDate} from '@/common/js/date';
+  import ratingselect from '@/components/ratingselect/ratingselect';
 
   const ALL = 2;
   const ERR_OK = 0;
@@ -98,6 +98,30 @@
       formatDate (time) {
         let date = new Date(time);
         return formatDate(date, 'yyyy-MM-dd hh:mm');
+      }
+    },
+    methods: {
+      needShow (type, text) {
+        if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
+      },
+      ratingtypeselect (type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      contenttoggle (onlyContent) {
+        this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
       }
     }
   };
@@ -205,4 +229,31 @@
               vertical-align: top
               font-size: 10px
               color: rgb(147, 153, 159)
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            color: rgb(7, 17, 27)
+            font-size: 12px
+          .recommend
+            line-height: 16px
+            font-size: 0
+            .icon-thumb_up, .item
+              display: inline-block
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              color: rgb(0, 160, 220)
+            .item
+              padding: 0 6px
+              border: 1px solid rgba(7, 17, 27, 0.1)
+              border-radius: 1px
+              color: rgb(147, 153, 159)
+              background: #fff
+          .time
+            position: absolute
+            top: 0
+            right: 0
+            line-height: 12px
+            font-size: 10px
+            color: rgb(147, 153, 159)
 </style>
